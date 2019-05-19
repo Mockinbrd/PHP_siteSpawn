db555f931922ecbc0ac4b009e924e4e5c90c7615
Mockingbrd <marvyn.ap@gmail.com>
2019-05-19 21:38:38 +0200

    [DONE] new_spawn.php

--- SiteSpawn/_scripts/new_spawn.php
+++ SiteSpawn/_scripts/new_spawn.php
@@ -3,7 +3,7 @@
 function new_spawn($db) {
 
     $request = $db->query('SELECT * FROM spawnsimages 
-                            WHERE rand() > 0.9 
+                             
                             ORDER BY rand() 
                             LIMIT 1');
     while ($data = $request->fetch())
