88b5928b0633a08faebb5617dcb31aae2c3a7bb2
Mockingbrd <marvyn.ap@gmail.com>
2019-05-19 21:34:01 +0200

    Amélioration de la fonction du tirage au sort

--- SiteSpawn/_scripts/new_spawn.php
+++ SiteSpawn/_scripts/new_spawn.php
@@ -2,7 +2,10 @@
 
 function new_spawn($db) {
 
-    $request = $db->query('SELECT * FROM spawnsimages WHERE `id` = ROUND( RAND() * 4 ) + 1');
+    $request = $db->query('SELECT * FROM spawnsimages 
+                            WHERE rand() > 0.9 
+                            ORDER BY rand() 
+                            LIMIT 1');
     while ($data = $request->fetch())
     {
         echo '<p>' . $data['link'] . '</p>';
